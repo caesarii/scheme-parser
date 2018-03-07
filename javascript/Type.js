b081c08e20ddc210115706b49fe70cd9fee7aa82
caesarii <iwangqinghe@gmail.com>
2018-03-07 16:45:37 +0800

    create type

---
+++ javascript/Type.js
@@ -0,0 +1,24 @@
+
+// 枚举类型
+class Type {
+    constructor() {
+        // auto 是单字符符号的类型
+        // this.auto = 'auto' // 0
+        // this.colon = 'colon' // 1
+        // this.comma = 'comma' // 2
+        // this.braceLeft = 'braceLeft' // 3
+        // this.braceRight = 'barceRight' //4
+        this.bracketLeft = 'bracketLeft' //5
+        this.bracketRight = 'bracketRight' //6
+        this.number = 'number' //7
+        this.string = 'string' //8
+        // this.token = 'token' //9
+        
+        this.operator = 'operator'
+        this.bool = 'bool'
+        this.comment = 'comment'
+        this.condition = 'condition'
+    }
+}
+
+module.exports = new Type()
